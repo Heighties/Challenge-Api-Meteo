4d4b435b72fd21cec6aef29feea7f7a1f5a76599
Heighties <heighties@gmail.com>
2022-06-08 18:39:53 +0200

    + api call function city

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // https://api.openweathermap.org/data/2.5/weather?q=Paris&lang=fr&appid=83a3a64d8c5c44db4351d1b9d6738aaa&units=metric
 const APIKEY = '83a3a64d8c5c44db4351d1b9d6738aaa'
 
-document.querySelector('#city').addEventListener('input',function(){
-        let url =`https://api.openweathermap.org/data/2.5/weather?q=Paris&lang=fr&appid=${APIKEY}&units=metric`;
+// document.querySelector('#city').addEventListener('input',function(){
+    let apiCall = function(city){
+        let url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&appid=${APIKEY}&units=metric`;
 
     fetch(url)
     .then((response) =>
@@ -16,5 +17,17 @@ document.querySelector('#city').addEventListener('input',function(){
         "<i class='fas fa-tint'></i>" + data.main.humidity + '%';
         document.querySelector('#wnd-block').innerHTML =
         "<i class='fas fa-wind'></i>" + data.main.speed + 'km/h';
-    }))
-});
+    })
+    ).catch(err => console.log('Erreur : ' + err));
+    }
+
+    document.querySelector('form').addEventListener('submit', function(e){
+        e.preventDefault();
+        let ville = document.querySelector('#input_city').value;
+
+        apiCall(ville);
+    });
+
+
+
+    // });
