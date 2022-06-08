3de2ab5cf6c655d886f780a288f13bd6eaf15c7f
Heighties <heighties@gmail.com>
2022-06-08 13:21:28 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// https://api.openweathermap.org/data/2.5/weather?q=Paris&lang=fr&appid=83a3a64d8c5c44db4351d1b9d6738aaa&units=metric
+const APIKEY = '83a3a64d8c5c44db4351d1b9d6738aaa'
+
+document.querySelector('#city').addEventListener('input',function(){
+        let url =`https://api.openweathermap.org/data/2.5/weather?q=Paris&lang=fr&appid=${APIKEY}&units=metric`;
+
+    fetch(url)
+    .then((response) =>
+    response.json()
+    .then((data) => {
+        console.log(data);
+        document.querySelector('#city-block').innerHTML = data.name;
+        document.querySelector('#tmp-block').innerHTML = 
+        "<i class='fas fa-thermometer-half'></i>" + data.main.temp + '°';
+        document.querySelector('#hmd-block').innerHTML =
+        "<i class='fas fa-tint'></i>" + data.main.humidity + '%';
+        document.querySelector('#wnd-block').innerHTML =
+        "<i class='fas fa-wind'></i>" + data.main.speed + 'km/h';
+    }))
+});
